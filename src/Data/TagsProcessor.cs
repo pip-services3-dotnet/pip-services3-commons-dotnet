6a6e1e24eb9e00eba419f9090d8c8ffe3c0ba356
Artyom Grishchenko <artyomgrishchenko@gmail.com>
2019-10-28 20:14:39 +0200

    Added TagsProcessor and tests

---
+++ src/Data/TagsProcessor.cs
@@ -0,0 +1,197 @@
+﻿using PipServices3.Commons.Reflect;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PipServices3.Commons.Data
+{
+	/// <summary>
+	/// Helper class to extract and process search tags from objects.
+	/// The search tags can be kept individually or embedded as hash tags inside text
+	/// like "This text has #hash_tag that can be used for search."
+	///  </summary>
+
+	public static class TagsProcessor
+	{
+		private const string NORMALIZE_REGEX = "(_|#)+";
+		private const string COMPRESS_REGEX = "( |_|#)+";
+		private const string SPLIT_REGEX = "(,|;)+";
+		private const string HASHTAG_REGEX = "#\\w+";
+
+		/// <summary>
+		/// Normalizes a tag by replacing special symbols like '_' and '#' with spaces.
+		/// When tags are normalized then can be presented to user in similar shape and form.
+		/// </summary>
+		/// <param name="tag">the tag to normalize.</param>
+		/// <returns>a normalized tag.</returns>
+		public static string NormalizeTag(string tag)
+		{
+			return tag != null
+				? Regex.Replace(tag, NORMALIZE_REGEX, " ").Trim()
+				: null;
+		}
+
+		/// <summary>
+		/// Compress a tag by removing special symbols like spaces, '_' and '#'
+		/// and converting the tag to lower case.
+		/// When tags are compressed they can be matched in search queries.
+		/// </summary>
+		/// <param name="tag">the tag to compress.</param>
+		/// <returns>a compressed tag.</returns>
+		public static string CompressTag(string tag)
+		{
+			return tag != null
+				? Regex.Replace(tag, COMPRESS_REGEX, "").ToLower()
+				: null;
+		}
+
+		/// <summary>
+		///  Compares two tags using their compressed form.
+		/// </summary>
+		/// <param name="tag1">the first tag.</param>
+		/// <param name="tag2">the second tag.</param>
+		/// <returns>true if the tags are equal and false otherwise.</returns>
+		public static bool EqualTags(string tag1, string tag2)
+		{
+			if (tag1 == null && tag2 == null)
+				return true;
+			if (tag1 == null || tag2 == null)
+				return false;
+			return CompressTag(tag1) == CompressTag(tag2);
+		}
+
+
+		/// <summary>
+		/// Normalizes a list of tags.
+		/// </summary>
+		/// <param name="tags">the tags to normalize.</param>
+		/// <returns>a list with normalized tags.</returns>
+		public static string[] NormalizeTags(string[] tags)
+		{
+			return tags.Select(NormalizeTag).ToArray();
+		}
+
+		/// <summary>
+		/// Normalizes a comma-separated list of tags.
+		/// </summary>
+		/// <param name="tagList">a comma-separated list of tags to normalize.</param>
+		/// <returns>a list with normalized tags.</returns>
+		public static string[] NormalizeTagList(string tagList)
+		{
+			var tags = Regex.Split(tagList, SPLIT_REGEX).ToList();
+			// Remove separators
+			for (var index = 0; index < tags.Count - 1; index++)
+				tags.Splice(index + 1, 1);
+
+			return NormalizeTags(tags.ToArray());
+		}
+
+		public static List<T> Splice<T>(this List<T> source, int index, int count)
+		{
+			var items = source.GetRange(index, count);
+			source.RemoveRange(index, count);
+			return items;
+		}
+
+
+		/// <summary>
+		/// Compresses a list of tags.
+		/// </summary>
+		/// <param name="tags">the tags to compress.</param>
+		/// <returns>a list with normalized tags.</returns>
+		public static string[] CompressTags(string[] tags)
+		{
+			return tags.Select(CompressTag).ToArray();
+		}
+
+
+		/// <summary>
+		/// Compresses a comma-separated list of tags.
+		/// </summary>
+		/// <param name="tagList">a comma-separated list of tags to compress.</param>
+		/// <returns>a list with compressed tags.</returns>
+		public static string[] CompressTagList(string tagList)
+		{
+			//var tags = tagList.split(this.SPLIT_REGEX);
+			var tags = Regex.Split(tagList, SPLIT_REGEX);
+
+			var tagsList = tags.ToList();
+			// Remove separators
+			for (var index = 0; index < tagsList.Count - 1; index++)
+				tagsList.Splice(index + 1, 1);
+
+			return CompressTags(tagsList.ToArray());
+		}
+
+
+
+		/// <summary>
+		/// Extracts hash tags from a text.
+		/// </summary>
+		/// <param name="text">a text that contains hash tags</param>
+		/// <returns>a list with extracted and compressed tags.</returns>
+		public static string[] ExtractHashTags(string text)
+		{
+			var tags = new string[0];
+
+			if (text != "")
+			{
+				string[] hashTags = (from Match match in Regex.Matches(text, HASHTAG_REGEX) select match.Value).ToArray();
+				tags = CompressTags(hashTags);
+			}
+
+			return tags.Distinct().ToArray();
+		}
+
+		private static string ExtractString(dynamic field)
+		{
+			if (field == null) return "";
+
+			if (field is string) return field.ToString();
+
+			var result = "";
+			if (field is Dictionary<string, string>)
+			{
+				foreach (string prop in field.Keys)
+				{
+					result += " " + ExtractString(field[prop]);
+				}
+
+				return result;
+			}
+
+			var properties = PropertyReflector.GetProperties(field);
+			foreach (string prop in properties.Keys)
+			{
+				result += " " + ExtractString(properties[prop]);
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Extracts hash tags from selected fields in an object.
+		/// </summary>
+		/// <param name="obj">an object which contains hash tags.</param>
+		/// <param name="searchFields">a list of fields in the objects where to extract tags</param>
+		/// <returns>a list of extracted and compressed tags.</returns>
+		public static string[] ExtractHashTagsFromValue(dynamic obj, params string[] searchFields)
+		{
+			// Todo: Use recursive
+			string[] tags = CompressTags(obj.Tags.ToArray());
+
+			foreach (string field in searchFields)
+			{
+				string text = ExtractString(PropertyReflector.GetProperty(obj, field));
+
+				if (text != "")
+				{
+					string[] hashTags = (from Match match in Regex.Matches(text, HASHTAG_REGEX) select match.Value).ToArray();
+					tags = tags.Concat(CompressTags(hashTags).ToList()).ToArray();
+				}
+
+			}
+
+			return tags.Distinct().ToArray();
+		}
+	}
+}
---
+++ test/Data/TagsProcessorTest.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace PipServices3.Commons.Data
+{
+	public class TagsProcessorTest
+	{
+		[Fact]
+		public void TestNormalizeTags()
+		{
+			var tag = TagsProcessor.NormalizeTag("  A_b#c ");
+			Assert.Equal("A b c", tag);
+
+			var tags = TagsProcessor.NormalizeTags(new[] { "  A_b#c ", "d__E f" });
+			Assert.Equal(2, tags.Length);
+			Assert.Equal("A b c", tags[0]);
+			Assert.Equal("d E f", tags[1]);
+
+			tags = TagsProcessor.NormalizeTagList("  A_b#c ,d__E f;;");
+			Assert.Equal(3, tags.Length);
+			Assert.Equal("A b c", tags[0]);
+			Assert.Equal("d E f", tags[1]);
+
+		}
+
+		[Fact]
+		public void TestCompressTags()
+		{
+			var tag = TagsProcessor.CompressTag("  A_b#c ");
+			Assert.Equal("abc", tag);
+
+			var tags = TagsProcessor.CompressTags(new[] { "  A_b#c ", "d__E f" });
+			Assert.Equal(2, tags.Length);
+			Assert.Equal("abc", tags[0]);
+			Assert.Equal("def", tags[1]);
+
+			tags = TagsProcessor.CompressTagList("  A_b#c ,d__E f;;");
+			Assert.Equal(3, tags.Length);
+
+			Assert.Equal("abc", tags[0]);
+			Assert.Equal("def", tags[1]);
+
+		}
+
+		[Fact]
+		public void TestExtractHashTags()
+		{
+			var tags = TagsProcessor.ExtractHashTags("  #Tag_1  #TAG2#tag3 ");
+			Assert.Equal(3, tags.Length);
+			Assert.Equal("tag1", tags[0]);
+			Assert.Equal("tag2", tags[1]);
+			Assert.Equal("tag3", tags[2]);
+		}
+
+		public class Test1
+		{
+			public List<string> Tags = new List<string> { "Tag 1", "tag_2", "TAG3" };
+			public string Name = "Text with tag1 #Tag1";
+			public string Description = "Text with #tag_2,#tag3-#tag4 and #TAG__5";
+		}
+
+		public class NameTest
+		{
+			public string Short = "Just a name";
+			public string Full = "Text with tag1 #Tag1";
+		}
+		public class DescriptionTest
+		{
+			public string En = "Text with #tag_2,#tag4 and #TAG__5";
+			public string Ru = "Текст с #tag_2,#tag3 и #TAG__5";
+		}
+		public class Test2
+		{
+			public List<string> Tags = new List<string> { "Tag 1", "tag_2", "TAG3" };
+			public NameTest Name = new NameTest();
+			public DescriptionTest Description = new DescriptionTest();
+		}
+		public class Test3
+		{
+			public List<string> Tags = new List<string> { "Tag 1", "tag_2", "TAG3" };
+			public Dictionary<string, string> Name = new Dictionary<string, string> { { "Text with tag1", "#Tag1" } };
+			public Dictionary<string, string> Description = new Dictionary<string, string> { { "Text with", " #tag_2,#tag3-#tag4 " }, { "Text with2", " and #TAG__5" } };
+		}
+		public class Test4
+		{
+			public List<string> Tags = new List<string> { "Tag 1", "tag_2", "TAG3" };
+			public List<string> Name = new List<string> { "Text with tag1", "#Tag1" };
+			public List<string> Description = new List<string> { "Text with #tag_2,#tag3-#tag4", " and #TAG__5" };
+		}
+
+
+
+		[Fact]
+		public void TestExtractHashTagsFromValue()
+		{
+			var tags = TagsProcessor.ExtractHashTagsFromValue(new TagsProcessorTest.Test1(), "Name", "Description");
+
+			var exp = new[] { "tag1", "tag2", "tag3", "tag4", "tag5" };
+			Assert.Equal(exp.Length, tags.Length);
+			for (int i = 0; i < exp.Length; i++) Assert.Equal(exp[i], tags[i]);
+		}
+
+		[Fact]
+		public void TestExtractHashTagsFromObject()
+		{
+			var tags = TagsProcessor.ExtractHashTagsFromValue(new TagsProcessorTest.Test2(), "Name", "Description");
+
+			var exp = new[] { "tag1", "tag2", "tag3", "tag4", "tag5" };
+			Assert.Equal(exp.Length, tags.Length);
+			for (int i = 0; i < exp.Length; i++) Assert.Equal(exp[i], tags[i]);
+
+		}
+
+		[Fact]
+		public void TestExtractHashTagsFromDictionary()
+		{
+			var tags = TagsProcessor.ExtractHashTagsFromValue(new TagsProcessorTest.Test3(), "Name", "Description");
+
+			var exp = new[] { "tag1", "tag2", "tag3", "tag4", "tag5" };
+			Assert.Equal(exp.Length, tags.Length);
+			for (int i = 0; i < exp.Length; i++) Assert.Equal(exp[i], tags[i]);
+
+		}
+
+		[Fact]
+		public void TestExtractHashTagsFromList()
+		{
+			var tags = TagsProcessor.ExtractHashTagsFromValue(new TagsProcessorTest.Test3(), "Name", "Description");
+
+			var exp = new[] { "tag1", "tag2", "tag3", "tag4", "tag5" };
+			Assert.Equal(exp.Length, tags.Length);
+			for (int i = 0; i < exp.Length; i++) Assert.Equal(exp[i], tags[i]);
+
+		}
+	}
+}
